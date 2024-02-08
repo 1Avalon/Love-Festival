7f09702b413730236c89841a17f6b9206780fbbc
Alex <axj30.11.04@gmail.com>
2024-02-08 19:13:57 +0100

    add logger

---
+++ Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveFestival
+{
+    public static class Logger
+    {
+        public static void Log_Warning(string message, bool debug = false) 
+        {
+            ModEntry.instance.Monitor.Log(message, StardewModdingAPI.LogLevel.Warn);
+            if (debug)
+                Debug.WriteLine(message);
+        }
+    }
+}
