9021e0e1f1a54cabea3c0fa6ac015c7fd87905fe
Avalon <axj30.11.04@gmail.com>
2024-02-11 16:07:17 +0100

    add moddate class

---
+++ ModDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveFestival
+{
+    public sealed class ModDate
+    {
+
+        public string condition;
+
+        public string eventScript;
+
+        public string mapName;
+
+        public string[] forks;
+
+        public string dateLetterContent;
+
+        public string acceptingDateResponse;
+
+        public ModDate(string condition, string eventScript, params string[] forks)
+        {
+            this.condition = condition;
+            this.eventScript = eventScript;
+            this.forks = forks;
+        }
+    }
+}
