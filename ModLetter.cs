6ee1239b1f7e71f026981c1404ae6a54e15153c0
Avalon <axj30.11.04@gmail.com>
2024-02-16 23:34:35 +0100

    init

---
+++ ModLetter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveFestival
+{
+    public abstract class ModLetter
+    {
+        public string Content;
+
+        public string Attachment = null;
+
+        public string ContentWithAttachment()
+        {
+            if (Attachment == null)
+                return Content;
+
+            return Content + "^^-NPCNAME < " + Attachment;
+        }
+    }
+
+    public static class Attachments
+    {
+        public static string Money(int money)
+        {
+            return $" %item money {money} %%";
+        }
+
+        public static string Money()
+        {
+            return $"%item money 1000 %%";
+        }
+        public static string Money(int moneyMin, int moneyMax)
+        {
+            return $"%item money {moneyMin} {moneyMax} %%";
+        }
+        public static string Item(int itemID, int amount = 1)
+        {
+            return $"%item object {itemID} {amount} %%";
+        }
+    }
+}
