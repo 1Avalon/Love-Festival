3a2cbc1b9734b4966c47f28ce699d6874ee25fa8
Avalon <axj30.11.04@gmail.com>
2024-04-03 20:14:20 +0100

    init

---
+++ FriendshipMultiplier.cs
@@ -0,0 +1,23 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveFestival
+{
+    public class FriendshipMultiplier
+    {
+        public string targetName;
+
+        public float amount = 1.2f;
+
+        public int expiresAt = ModEntry.festivalDate + 7;
+
+        public FriendshipMultiplier(string targetName) 
+        { 
+            this.targetName = targetName;
+        }
+    }
+}
