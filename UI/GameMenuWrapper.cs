1c38fbb99ec4011f863188128fb431de60e5ba48
Avalon <axj30.11.04@gmail.com>
2024-04-04 19:26:11 +0100

    change namespace

--- UI/GameMenuWrapper.cs
+++ UI/GameMenuWrapper.cs
@@ -9,7 +9,7 @@ using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LoveFestival
+namespace LoveFestival.UI
 {
     public class GameMenuWrapper : GameMenu
     {
